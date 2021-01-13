75583e3cf8153c06ea47a514da243369c2bd42d3
Jason <hymciro_led@163.com>
2021-01-14 00:10:19 +0800

    [JAVA practice] 2021/1/14

--- solution.java
+++ solution.java
@@ -6,6 +6,7 @@ interface canFly{
 interface canSwim{
     void swim();
 }
+
 class dragon implements canFly, canSwim{
     public void fly(){
         System.out.println("dragon fly");
@@ -17,13 +18,77 @@ class dragon implements canFly, canSwim{
     public void swim(){
         System.out.println("dragon swim");
     }
+    int i = 1;
+    void tip() {
+        System.out.println(i);
+    }
 }
+
+class tiger {
+    static String name = "tiger";
+    int size = 20;
+    class hunter {
+        void tes() {
+            System.out.println(name);
+            System.out.println(size);
+        }
+    }
+    void tes() {
+        System.out.println(name);
+        System.out.println(size);
+    }
+}
+
+class lion extends tiger {
+    class hunter {
+        void tes() {
+            System.out.println("lion hunter");
+        }
+    }
+}
+
+class cat {
+    class tail {}
+    static class claw {}
+    canSwim getFat() {
+        return new canSwim() {
+            public void swim() {
+                System.out.println("cat.swim");
+            }
+        };
+    }
+    void getThin() {
+        class minicat {
+            minicat() {
+                System.out.println("getThin.minicat");
+            }
+        }
+        new minicat();
+    }
+}
+
+class color_tail extends cat.tail {
+    color_tail(cat my_cat) {
+        my_cat.super();
+    }
+}
+
+
+
 public class solution{
     public static void main(String[] args){
-        dragon ethla = new dragon();
-        ethla.fly();
-        ethla.swim();
-        ethla.swim("jlk");
-        System.out.println(ethla.m);
+        // tiger jone = new tiger();
+        // System.out.println(jone.name);
+        // System.out.println(jone.size);
+        // tiger.hunter mary = jone.new hunter();
+        // mary.tes();
+
+        // lion page = new lion();
+        // System.out.println(page.name);
+        // System.out.println(page.size);
+        // lion.hunter bob = page.new hunter();
+        // bob.tes();
+        dragon dito = new dragon();
+        dito.tip();
     }
 }
